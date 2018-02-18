cb3a768daee437d8b81959a88a8f3cb4a4a20d73
Taylor Growden <taylorgrowden@gmail.com>
2018-02-18 17:09:27 -0500

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-const config = {
+module.exports = {
 	entry: {
 		index: path.join(__dirname, 'src', 'index.js')
 	},
@@ -36,59 +35,5 @@ const config = {
 	resolve: {
 		extensions: ['.json', '.js', '.jsx', '.css']
 	},
-	devtool: 'source-map',
-	devServer: {
-		contentBase: path.join(__dirname, 'docs'),
-		compress: true,
-		port: 9000,
-		publicPath: path.join(__dirname, 'docs')
-	}
+	devtool: 'source-map'
 }
-
-const exampleConfig = {
-	entry: {
-		example: path.join(__dirname, 'example', 'index.js')
-	},
-	output: {
-		filename: '[name].js',
-		path: path.resolve(__dirname, 'docs')
-	},
-	module: {
-		rules: [{
-			test: /.jsx?$/,
-			include: [
-				path.resolve(__dirname, 'src'),
-				path.resolve(__dirname, 'example')
-			],
-			exclude: [
-				path.resolve(__dirname, 'node_modules')
-			],
-			loader: 'babel-loader',
-			query: {
-				presets: [
-					'es2015',
-					'react'
-				]
-			}
-		}]
-	},
-	resolve: {
-		extensions: ['.json', '.js', '.jsx', '.css']
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			title: 'Material-UI Autosuggest Example',
-			template: path.join(__dirname, 'example', 'index.html'),
-			inject: false
-		})
-	],
-	devtool: 'source-map',
-	devServer: {
-		contentBase: path.join(__dirname, 'docs'),
-		compress: true,
-		port: 9000,
-		publicPath: path.join(__dirname, 'docs')
-	}
-}
-
-module.exports = [ config, exampleConfig ]
